8baf2b34731a22886dd2eb93aff69cfe95d4d0ff
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-05-27 21:13:41 -0500

    added second page which lists all contacts on one page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,9 @@ class PeopleController < ApplicationController
   def contacts
     @name = Person.first
   end
+
+  def all
+    @names = Person.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/contacts" => 'people#contacts'
+
+  get "/all" => 'people#all'
 end
