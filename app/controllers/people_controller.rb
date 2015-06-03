571740fec538f3a247884ba36ced3c72b871d152
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-06-02 19:05:05 -0500

    added all remaining RESTful actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,6 +12,28 @@ class PeopleController < ApplicationController
   end
 
   def create
+    Person.create(name: params[:name], email_address: params[:email_address], mailing_address: params[:mailing_address], mobile_number: params[:mobile_number], notes: params[:notes])
+  end
+
+  def show
+    @name = Person.find_by(id: params[:id])
+  end
+
+  def edit
+    contact_id = params[:id]
+    @name = Person.find_by(id: contact_id)
+  end
+
+  def update
+    contact_id = params[:id]
+    @name = Person.find_by(id: contact_id)
+    @name.update(name: params[:name], email_address: params[:email_address], mailing_address: params[:mailing_address], mobile_number: params[:mobile_number], notes: params[:notes])
+  end
+
+  def destroy
+    contact_id = params[:id]
+    @name = Person.find_by(id: contact_id)
+    @name.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   get "/" => 'people#index'
   get "/contacts/new" => 'people#new'
   post "/contacts" => 'people#create'
+  get "/contacts/:id/edit" => 'people#edit'
+  get "/contacts/:id" => 'people#show'
+  patch "contacts/:id" => 'people#update'
+
+  delete "contacts/:id" => 'people#destroy'
 
 end
